52b15f62871160cf427cf09286708d6bd23f4975
renaldi24 <49301754+renaldi24@users.noreply.github.com>
2019-07-02 21:12:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>".count($registrants)."people";
+                echo "<h2>People who are registered: ".count($registrants)." people</h2>";
                 echo "<table>";
                 echo "<tr><th>Name</th>";
                 echo "<th>Email</th>";
